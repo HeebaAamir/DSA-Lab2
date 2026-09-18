d462ca12d6790c1ba94f451730c64d3370dca37a
HeebaAamir <heeba.aamir@gmail.com>
2026-09-18 19:17:33 +0500

    Added the function declaration for isSorted function

--- test_driven_development.cpp
+++ test_driven_development.cpp
@@ -2,7 +2,14 @@
 #include <assert.h> // header file for assert function
 using namespace std;
 
-bool isSorted(const int *arr, const int size){}
+bool isSorted(const int *arr, const int size){
+    for(int i=0; i<size;i++){
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
 
 void testSortedArray() { 
     int arr[] = {1, 2, 3, 4, 5}; 
